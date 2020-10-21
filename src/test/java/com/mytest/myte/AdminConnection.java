d1d166f115644afa20f76810ff8f5cd429825375
KW505 <KW505@505-10>
2020-10-21 16:10:33 +0900

    스프링 테스트입니다.

---
+++ src/test/java/com/mytest/myte/AdminConnection.java
@@ -0,0 +1,55 @@
+package com.mytest.myte;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
+public class AdminConnection implements InitializingBean,DisposableBean {
+	
+	private String adminId;
+	private String adminPw;
+	private String sub_adminId;
+	private String sub_adminPw;
+	
+	
+	
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminPw() {
+		return adminPw;
+	}
+	public void setAdminPw(String adminPw) {
+		this.adminPw = adminPw;
+	}
+	public String getSub_adminId() {
+		return sub_adminId;
+	}
+	public void setSub_adminId(String sub_adminId) {
+		this.sub_adminId = sub_adminId;
+	}
+	public String getSub_adminPw() {
+		return sub_adminPw;
+	}
+	public void setSub_adminPw(String sub_adminPw) {
+		this.sub_adminPw = sub_adminPw;
+	}
+	
+	
+	
+	
+}
